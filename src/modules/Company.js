4dc8beb523e515c41c42fe63b498e433f96ac783
kenji <gunsecondstage0106@gmail.com>
2017-10-30 13:07:49 +0900

    遷移先の画面を整形した。

--- src/modules/Company.js
+++ src/modules/Company.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { 
-  Image, 
-  Text, 
-  View
+import PropTypes from 'prop-types';
+import {
+    ScrollView,
+    Image,
+    Text,
+    View
 } from 'react-native';
 
 import styles from './styles/Company';
 
-export default class Company extends Component {
+class Company extends Component {
     constructor(props) {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this._onNavigatorEvent.bind(this));
@@ -21,18 +23,59 @@ export default class Company extends Component {
         }
     }
 
+    _onScroll(event) {
+        const offSetY = event.nativeEvent.contentOffset.y.toFixed();
+        if (offSetY > 130) {
+            this._toggleNavbar('hidden');
+        } else {
+            this._toggleNavbar('shown');
+        }
+    }
+
+    _toggleNavbar(status) {
+        this.props.navigator.toggleNavBar({
+            to: status,
+            animated: true
+        });
+    }
+
     render() {
         const { company } = this.props;
         return (
-            <View>
-                <Image source={require('../../images/coffee.png')} style={styles.image} />
-                    <Text style={styles.cardText} numberOfLines={3}>
-                        {company.name}
-                    </Text>
-                    <Text style={styles.cardText} numberOfLines={10}>
-                        {company.overview}
-                    </Text>
-            </View>
-        )
+            <ScrollView
+                style={styles.container}
+                onScroll={this._onScroll.bind(this)}>
+
+                <View style={styles.itemContainer}>
+                    <Image source={require('../../images/coffee.png')} style={styles.image} />
+                    <View style={styles.overviewArea}>
+                        <Text style={styles.title}>{company.name}</Text>
+                        <Text style={styles.overview}>{company.overview}</Text>
+                    </View>
+                </View>
+                <View style={styles.contentArea}>
+                    <View style={styles.contentContainer}>
+                        <View style={styles.labelRow}>
+                            <Text style={styles.label}>[従業員数]</Text>
+                            <Text style={styles.value}>{company.employeesNum}名</Text>
+                        </View>
+                        <View style={styles.labelRow}>
+                            <Text style={styles.label}>[給与]</Text>
+                            <Text style={styles.value}>{company.salaryLow}万円〜{company.salaryHigh}万円</Text>
+                        </View>
+                        <View style={styles.labelRow}>
+                            <Text style={styles.label}>[URL]</Text>
+                            <Text style={styles.value}>{company.url}</Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        );
     }
 }
+
+Company.propTypes = {
+	company: PropTypes.object.isRequired
+};
+
+export default Company;
--- src/modules/styles/Company.js
+++ src/modules/styles/Company.js
@@ -1,12 +1,73 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black'
+    },
+    itemContainer: {
+        flex: 1,
+        top: 20,
+        right: 16,
+        left: 16,
+        flexDirection: 'row'
+    },
     image: {
         height: 150,
         width: 110,
         borderTopLeftRadius: 3,
         borderBottomLeftRadius: 3
-    }
+    },
+    overviewArea: {
+        flex: 1,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 20
+    },
+    title: {
+        color: 'white',
+		fontSize: 19,
+		fontWeight: '500',
+		paddingTop: 10
+    },
+    overview: {
+        color: 'white',
+        fontSize: 15,
+        paddingTop: 20
+    },
+    contentArea: {
+        flex: 1,
+        marginTop: 20
+    },
+    tabText: {
+        color: 'white',
+        paddingTop: 10,
+        fontSize: 12,
+        fontWeight: 'bold'
+    },
+    underline: {
+        backgroundColor: '#FFFF00'
+    },
+    tabBar: {
+        backgroundColor: 'white'
+    },
+    contentContainer: {
+		paddingTop: 20,
+		paddingHorizontal: 16,
+        paddingBottom: 25
+    },
+    labelRow: {
+		flexDirection: 'column',
+		paddingTop: 10
+	},
+	label: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: '500'
+	},
+	value: {
+		color: 'white',
+		fontSize: 14
+	}
 });
 
 export default styles;
