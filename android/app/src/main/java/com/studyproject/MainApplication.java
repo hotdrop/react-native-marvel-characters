f035330a3f0517b6be196ae061384311e1a5b437
kenji <gunsecondstage0106@gmail.com>
2017-11-07 16:15:37 +0900

    Androidのコメントコードを削除

--- android/app/src/main/java/com/studyproject/MainApplication.java
+++ android/app/src/main/java/com/studyproject/MainApplication.java
@@ -1,17 +1,10 @@
 package com.studyproject;
 
-//import android.app.Application;
 import com.reactnativenavigation.NavigationApplication;
-
-//import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-//import com.reactnativenavigation.NavigationReactPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-//import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-//import com.facebook.react.shell.MainReactPackage;
-//import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,37 +28,4 @@ public class MainApplication extends NavigationApplication {
   public List<ReactPackage> createAdditionalReactPackages() {
       return getPackages();
   }
-
-  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-  //   @Override
-  //   public boolean getUseDeveloperSupport() {
-  //     return BuildConfig.DEBUG;
-  //   }
-
-  //   @Override
-  //   protected List<ReactPackage> getPackages() {
-  //     return Arrays.<ReactPackage>asList(
-  //         new MainReactPackage(),
-  //           new VectorIconsPackage(),
-  //           new NavigationReactPackage(),
-  //           new ReactNativeConfigPackage()
-  //     );
-  //   }
-
-  //   @Override
-  //   protected String getJSMainModuleName() {
-  //     return "index";
-  //   }
-  // };
-
-  // @Override
-  // public ReactNativeHost getReactNativeHost() {
-  //   return mReactNativeHost;
-  // }
-
-  // @Override
-  // public void onCreate() {
-  //   super.onCreate();
-  //   SoLoader.init(this, /* native exopackage */ false);
-  // }
 }
